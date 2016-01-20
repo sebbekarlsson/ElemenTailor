d7d2efa73db8d321fe34e33ad4bb2772ddb69c93
Sebastian karlsson <sebastiankarlsson@Sebastians-MacBook-Air.local>
2016-01-20 10:39:02 +0100

    Updated example

--- example.js
+++ example.js
@@ -3,7 +3,8 @@ var dom = ElemenTailor.create(
         {
             childs: [
                 ElemenTailor.create('section', {childs: [
-                    ElemenTailor.create('img', {class: 'beautiful-image', innerHTML: 'test'})
+                    ElemenTailor.create('img', {class: 'beautiful-image'}),
+                    ElemenTailor.create('span', {innerHTML: 'I am a text!'})
                 ]})
             ]
         },
